ab8902fa973d5175b350ea3903f4c94a8834b474
adamskibicki <adam-skibicki@outlook.com>
2019-06-20 09:51:47 +0200

    Prepared configuration with profiles

--- PasswordManager/Program.cs
+++ PasswordManager/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration.Binder;
 
 namespace PasswordManager
 {
@@ -6,7 +11,24 @@ namespace PasswordManager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+                .Build();
+
+            DisplayConfiguration displayConfiguration = configuration.Get<DisplayConfiguration>();
+
+            Console.WriteLine(displayConfiguration.AppDisplayName);
+
+            Console.ReadKey();
         }
+    }
+
+    public class DisplayConfiguration
+    {
+        public string AppDisplayName { get; set; }
     }
 }
